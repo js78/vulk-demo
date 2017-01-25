2163abeb6e29c9603b4cbb7238bec38d5d212ed4
Jean-Sebastien Bevilacqua <realitix@gmail.com>
2017-01-25 19:39:04 +0100

    Update test

--- vulkdemo/__init__.py
+++ vulkdemo/__init__.py
@@ -13,24 +13,33 @@ class App(BaseApp):
         super().start()
         path = os.path.dirname(os.path.abspath(__file__))
 
-        self.spritebatch = SpriteBatch(self.context, clear=[1, 1, 1, 1])
+        # self.spritebatch = SpriteBatch(self.context, clear=[1, 1, 1, 1])
+        self.spritebatch = SpriteBatch(self.context)
 
         # ----------
         # TEST TEXTURE
-        self.texture = Texture(self.context, os.path.join(path, 'vulkan.png'))
+        vulkan_path = os.path.join(path, 'vulkan.png')
+        starwars_path = os.path.join(path, 'starwars.jpg')
+        self.texture = Texture(self.context, vulkan_path)
+        self.texture2 = Texture(self.context, starwars_path)
         self.x = 0
         self.y = 0
+        self.size = 100
 
     def end(self):
         pass
 
     def render(self, delta):
+        self.context.clear_final_image([1, 1, 1, 1])
         speed = 1
         self.x += speed * delta
         self.y += speed * delta
+        self.size += speed * delta
 
         self.spritebatch.begin(self.context)
-        self.spritebatch.draw(self.texture, self.x, self.y, 100, 100)
-        spritebatch_semaphore = self.spritebatch.end(self.context)
+        self.spritebatch.draw(self.texture2, 0, 0, self.size, self.size)
+        self.spritebatch.draw(self.texture, self.context.width - self.size,
+                              0, self.size, self.size)
+        spritebatch_semaphore = self.spritebatch.end()
 
         self.context.swap([spritebatch_semaphore])
