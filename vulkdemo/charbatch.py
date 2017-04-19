c88a0231839bfeb2009a086aaac5000eab8c370b
Jean-Sebastien Bevilacqua <realitix@gmail.com>
2017-04-19 08:39:17 +0200

    Update char batch

--- vulkdemo/charbatch.py
+++ vulkdemo/charbatch.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.6
 from vulk.baseapp import BaseApp
-from vulk.graphic.d2.batch import TextBatch
+from vulk.graphic.d2.batch import CharBatch
 from vulk.graphic.d2.fontdata import FontData
 
 
@@ -11,7 +11,7 @@ class App(BaseApp):
     def start(self):
         super().start()
         self.data = FontData(self.context, '/home/realitix/git/vulk/vulk/asset/font/arial.fnt')
-        self.batch = TextBatch(self.context)
+        self.batch = CharBatch(self.context)
 
 
     def end(self):
@@ -25,7 +25,7 @@ class App(BaseApp):
         self.context.clear_final_image([0, 0, 0.2, 1])
 
         self.batch.begin(self.context)
-        self.batch.draw_char(self.data, "t", 10, 10, 100, 100)
+        self.batch.draw_char(self.data, "a", 10, 10, scale_x=0.5, scale_y=0.5)
         batch_semaphore = self.batch.end()
         self.context.swap([batch_semaphore])
 
